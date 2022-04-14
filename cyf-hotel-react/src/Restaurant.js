50cfbf786ecc5e1e7320994d86ff7d2af7314228
DelroyGayle <delroygayle01@gmail.com>
2022-04-14 13:44:16 +0100

    added autoComplete

---
+++ cyf-hotel-react/src/Restaurant.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Order from "./Order";
+
+const Restaurant = () => {
+  return (
+    <div>
+      <h3>Restaurant Orders</h3>
+      <ul>
+        <Order orderType="Pizzas" />
+        <Order orderType="Salads" />
+        <Order orderType="Chocolate Cakes" />
+      </ul>
+    </div>
+  );
+};
+
+export default Restaurant;
